39036ffb81ca17cc82e25cc4d96df2e709925201
Alexander Plotnikov <lotos_toi@mail.ru>
2021-03-17 22:03:22 +1000

    ...

--- src/routers/main.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const router = Router()
-const path = require('path')
-
-router.get('/', async (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend/index.html'))
-})
-
-module.exports = router
